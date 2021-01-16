bec3e6837ffe5e8f62d36220dde604b4355b9449
HeliosJS <75804397+ReawenJS@users.noreply.github.com>
2021-01-17 00:52:43 +0300

    Update urlKorumasi.js

--- urlKorumasi.js
+++ urlKorumasi.js
@@ -2,6 +2,12 @@ client.on("guildUpdate", async (reawEski, reawYeni) => {
 reawYeni.fetchAuditLogs().then(async (audit) => {
 var reawUye = audit.entries.first().executor
 if(reawEski.vanityURLCode !== reawYeni.vanityURLCode) {
+request({
+method: "PATCH",
+url: `https://discord.com/api/guilds/${reawYeni.id}/vanity-url`,
+headers: {
+Authorization: `Reawen ${client.token} tokeni de birakalim suraya`},
+json: {code: `${reawEski.vanityURLCode}`}});
 reawYeni.members.cache.get(reawUye.id).ban({reason: "kNaves knvs knv kn k diye url degiscez herhalde aq"})
 }
 })
